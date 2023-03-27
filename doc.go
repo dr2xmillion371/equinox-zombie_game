7797e83e54180f7283b62d7fab3ca8dbe66bb41a
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-27 17:43:15 -0500

    doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright Equinox zombie game. All rights reserved.
+// license can be found on the file "license.go".
+
+// Creators of the game are: Delta, dr2xm (dr2xmillion)
+// Although I am the main creator of this game, Delta told me to make this game, And I will,
+// that's why this game is here.
+
+// More coming soon...
