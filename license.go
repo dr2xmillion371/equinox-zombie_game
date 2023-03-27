e8d85784d88be82e563aebc24c9923c802459e9a
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-27 17:44:50 -0500

    license

---
+++ license.go
@@ -0,0 +1,3 @@
+// In order to copy this game, you will need to email me (my email is: romantengh@gmail.com)
+// Then I will either say yes or no depending on how you want to copy this game.
+// that's all
