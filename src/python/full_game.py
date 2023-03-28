72a862332bc72904d995ca8da161784533e55210
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-28 17:30:48 -0500

    full_game

---
+++ src/python/full_game.py
@@ -0,0 +1,17 @@
+print('Welcome to Equinox zombie!')
+users_opt = input('Want to play? (Y)es or (N)o: ')
+if users_opt == "Y":
+  print('Ok, let\'s start!')
+if users_opt == "N":
+  print('Ok, you don\'t have to play')
+if users_choice == "Kill":
+  print('Zombie attacked!')
+  print('Zombie dead!')
+if users_choice == "Leave alone":
+  print('Ok, leaving the zombie alone.')
+if users_choice == "Don't do anything":
+  print('Okay')
+if users_que == "14":
+    print('That\'s correct!')
+  if users_que != "14":
+    print('Whoops, that\'s incorrect!')
